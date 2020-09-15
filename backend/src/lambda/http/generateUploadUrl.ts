66ce17d00901e0238d68ebacaafedc75beff5e2f
Uzoamaka126 <vanessapeters126@gmail.com>
2020-09-15 15:27:23 +0100

    Todo: complete the generateUploadUrl function

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,11 +1,36 @@
 import 'source-map-support/register'
+import { createLogger } from '../../utils/logger';
+import * as middy from 'middy';
+import { generateUploadUrl } from '../../businessLogic/todosLogic';
+
+const logger = createLogger('update-a-todo');
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const todoId = event.pathParameters.todoId
-
-  console.log(todoId);
+export const handler: APIGatewayProxyHandler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
-}
+  logger.info('Processing event', event);
+  
+  try {
+    const todoId = event.pathParameters.todoId
+    const uploadUrl = await generateUploadUrl(todoId)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        uploadUrl
+      })
+    }
+  } catch (err) {
+    logger.error('Error: ' + err.message)
+
+    return {
+      statusCode: 500,
+      body: err.message
+    }
+  }
+})
