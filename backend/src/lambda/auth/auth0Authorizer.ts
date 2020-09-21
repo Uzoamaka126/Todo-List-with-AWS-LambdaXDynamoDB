d7f6dd92b548b8aebf7d9aed736c4029fb07cccd
Uzoamaka126 <vanessapeters126@gmail.com>
2020-09-21 22:46:28 +0100

    configure lambda auth file

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -2,10 +2,8 @@ import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 import { decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-// import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload';
-// import { Jwt } from '../../auth/Jwt'
 // import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth');
@@ -54,7 +52,7 @@ export const handler = async (
   }
 }
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
+  const token = getToken(authHeader);
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
   logger.info('jwd....', jwt);
   return jwt.payload;
@@ -63,10 +61,10 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
 function getToken(authHeader: string): string {
   if (!authHeader) throw new Error('No authentication header')
   if (!authHeader.toLowerCase().startsWith('bearer '))
-    throw new Error('Invalid authentication header')
-  const split = authHeader.split(' ')
-  const token = split[1]
-  return token
+    throw new Error('Invalid authentication header');
+  const split = authHeader.split(' ');
+  const token = split[1];
+  return token;
 }
 
 // export const handler = async (
